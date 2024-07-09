b6d0037e30ad3a93513332064aba5c267c1689ce
Mark Calcagno <mcalcagn@gmail.com>
2024-07-09 14:12:26 -0500

    transitions should only happen once now. Changed js to remove hidden

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
+            entry.target.classList.remove('hidden');
         }
     });
 });
